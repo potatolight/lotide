b1d8628eb57beed628783f48a5e5555d81723abe
potatolight <shuguangbai@gmail.com>
2020-07-21 04:11:21 +0000

    template literals1

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,7 @@ const assertEqual = function (actual, expected){
   if(actual === expected){
     console.log('😄😄Assertion Passed: '+actual+ '===' +expected);
   }else{
-    console.log('😢Assertion Failed: ' + actual+ '!==' +expected);
+    console.log(`😢Assertion Failed: ${actual} !== ${expected}`);
   }
 }
 assertEqual("Lighthouse Labs", "Bootcamp");
