b4f45abd79b1f02fa4524be41c7f9a93f03447c4
potatolight <shuguangbai@gmail.com>
2020-07-21 05:14:06 +0000

    tail

---
+++ tail.js
@@ -0,0 +1,11 @@
+const tail = function (words) {
+  let arr = words.slice(1);
+  console.log(arr);
+};
+const assertEqual = function(actual, expected) {
+  let len = actual === expected;
+  console.log(len);
+};
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
