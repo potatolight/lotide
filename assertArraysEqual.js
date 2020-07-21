584026c7b541017aca5350012685063852e6878d
potatolight <shuguangbai@gmail.com>
2020-07-21 23:18:29 +0000

    compare two arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+const assertArraysEqual = function (arr1, arr2) {
+  let len = arr1.length === arr2.length
+  let isMatch = true 
+  for(let i=0; i< arr1.length; i++){
+    if(arr1[i] !== arr2[i]){
+      isMatch = false 
+      break;
+    }
+  }
+  if(isMatch && len){
+    console.log('Passed: {firstArray: ['+arr1 +"]; secondArray: ["+arr2+'] firstArray is equal to secondArray}')
+  }else{
+    console.log('Failed: {firstArray: ['+arr1 +"]; secondArray: ["+arr2+'] firstArray is not equal to secondArray}')
+  }
+}
+assertArraysEqual([1,2],[3,4])
+assertArraysEqual(['good','luck'],['good','luck'])
