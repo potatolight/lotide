2b8b8be5260ba5b71d2522fbf7ce1dd713069a1e
Grace Bai <shuguangbai@gmail.com>
2020-07-22 15:09:50 -0700

    findKeyValue

---
+++ findKeyValue.js
@@ -0,0 +1,22 @@
+const findKeyByValue = function (obj, value) {
+  for(let key in obj){
+    if(obj[key] === value){
+      return key
+    }
+  }
+}
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('😄😄Assertion Passed: ' + actual + '===' + expected);
+  } else {
+    console.log(`😢Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
