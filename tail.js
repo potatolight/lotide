5e3a2b756ef438f3c980dd62059297a83d396ca4
potatolight <shuguangbai@gmail.com>
2020-07-27 21:10:18 +0000

    tail-refactor

--- tail.js
+++ tail.js
@@ -2,10 +2,12 @@ const tail = function (words) {
   let arr = words.slice(1);
   console.log(arr);
 };
-const assertEqual = function(actual, expected) {
-  let len = actual === expected;
-  console.log(len);
-};
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+// const assertEqual = function(actual, expected) {
+//   let len = actual === expected;
+//   console.log(len);
+// };
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
+
+module.exports = tail
