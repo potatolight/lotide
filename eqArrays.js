c5fd68677981d75fc6404469e2c3359ff9bd5ff8
potatolight <shuguangbai@gmail.com>
2020-07-27 21:30:49 +0000

    refactor-eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -51,6 +51,4 @@ const eqArrays = function (arr1, arr2) {
   return false
 }
 
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]))// => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
+module.exports = eqArrays;
