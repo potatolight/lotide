7fa1668d4dba23227ef09288e8e3df53ad1fc5eb
Grace Bai <shuguangbai@gmail.com>
2020-07-27 16:07:52 -0700

    refactor-middle-and-tailtest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,15 +1,24 @@
-const assertArraysEqual = require("../assertArraysEqual")
+
 const middle = require("../middle")
+const assert = require("chai").assert
 
 
-let arr1 = middle([1,2,3,4])
-let arr2 = [2, 3]
-assertArraysEqual(arr1, arr2)
+// let arr1 = middle([1,2,3,4])
+// let arr2 = [2, 3]
+// assertArraysEqual(arr1, arr2)
 
-let arr3 = middle([1, 2, 3, 4, 5])
-let arr4 = [3]
-assertArraysEqual(arr3, arr4)
+// let arr3 = middle([1, 2, 3, 4, 5])
+// let arr4 = [3]
+// assertArraysEqual(arr3, arr4)
 
-let arr5 = middle([1,2])
-let arr6 = []
-assertArraysEqual(arr5, arr6)
+// let arr5 = middle([1,2])
+// let arr6 = []
+// assertArraysEqual(arr5, arr6)
+describe("middle function", () => {
+  it("should return the array with only one middle number when the input's length is odd", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3])
+  });
+  it("should return the the array with two middle number when the input's length is even", () =>{
+   assert.deepEqual(middle([1,2,3,4]),[2, 3])
+  });
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,22 @@
 const tail = require("../tail")
-const assertEqual = require("../assertEqual")
+// const assertEqual = require("../assertEqual")
+const assert = require("chai").assert
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe ("tail Function" , function (){
+  it("should return the length of input arr", () => {
+    let input = [1, 2, 3]
+    assert.strictEqual(input.length, 3)
+  });
+  it("should return the rest of the word except the first one", () =>{
+    let input = ["Yo Yo", "Lighthouse", "Labs"]
+    let output  = ["Lighthouse", "Labs"]
+    assert.deepEqual(tail(input), output)
+  });
+  it("should return undefined when the input is not defined",()=>{
+    assert.strictEqual(tail(), null)
+  });
+  it("should return [] when the input only has one item",()=>{
+    assert.deepEqual(tail([1]), [])
+  })
+
+});
